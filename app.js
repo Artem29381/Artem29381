5c17acc7cb6168582dc6491fd76842ed30dc479a
Artem29381 <49292215+Artem29381@users.noreply.github.com>
2019-08-25 15:01:41 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,54 +1,77 @@
-let burg = document.getElementById('navClick');
-let nav = document.querySelector('.nav');
-burg.onclick = function() {
-	nav.classList.toggle('show');
-	nav.style.borderRadius = 5 + 'px';
-		if (document.body.offsetWidth < 1000) {
-		nav.style.height = 200 + 'px';}
-		else {
-			nav.style.height = 50 + 'px';
+$(function(){
+	/*Fixed Header */
+	let header = $("#header");
+	let intro = $("#intro");
+	let introHeight = intro.innerHeight();
+	let scrollPosition = $(window).scrollTop();
+	checkScroll(scrollPosition, introHeight);
+
+	$(window).on("scroll resize", function() {
+		//можно load resize добавить
+		scrollPosition = $(this).scrollTop();
+		checkScroll(scrollPosition, introHeight);
+	});
+
+function checkScroll(scrollPosition, introHeight) {
+		if (scrollPosition > introHeight) {
+			header.addClass('fixed')
 		}
-}
-
-
-let header = document.querySelector('.header');
-let intro = document.querySelector('.intro');
-
-window.addEventListener('scroll', function() {
-	if (window.pageYOffset > intro.offsetHeight) {
-		nav.style.position = 'fixed';
-		nav.style.right = 0;
-		nav.style.backgroundColor = '#333';
-		nav.style.top = 0;
-		nav.style.borderRadius = 5 + 'px';
-		if (document.body.offsetWidth < 1000) {
-		nav.style.height = 200 + 'px';}
 		else {
-			nav.style.height = 50 + 'px';
-		}
-	}
-	else {
-		nav.style.position = '';
-	}
+			header.removeClass('fixed')
+}}
+
+
+	/*Smooth scroll */
+
+	$("[data-scroll]").on("click", function(event) {
+		event.preventDefault(); /*Отменяет стандартное поведение ссылки */
+
+		let elementId = $(this).data('scroll');
+		let elementOffset = $(elementId).offset().top; //отступ данного элемента от вверха 
+		nav.removeClass('show');
+
+		$("html, body").animate({
+			scrollTop: elementOffset-60
+		}, 700);
+
+
+	});
+/*Burger menu click show */
+	let nav = $("#nav");
+	let navClick = $("#navClick");
+	navClick.on("click", function(event) {
+		event.preventDefault();
+		nav.toggleClass('show'); /*добавляет класс при клике, убираем при повторном клике*/
+
+	});
+
+
+	/*Reviews*/ 
+//https://kenwheeler.github.io/slick/
+
+let slider = $("#reviewsSlider");
+slider.slick({
+  infinite: true, //бесконечная прокрутка включена
+  slidesToShow: 1, //сколько скролим
+  slidesToScroll: 1, //сколько скроллим при клике
+  fade: true, //затемнение отзывов
+  arrows: false,
+  dots: true,
+  cssEase: 'linear',
+  autoplay: true,
+  autoplaySpeed: 3000,
+  pauseOnHover: false,
+  pauseOnFocus: false
+  
 });
-function scrollToWindow() {
-	let coordinates = this.getAttribute('coord');
-	let CorrectElement = document.getElementById(coordinates);
-	let Otstyp = CorrectElement.getBoundingClientRect().top;
-	CorrectElement.scrollIntoView({behavior: "smooth"});
-/*	if (Otstyp > 1) {
 
-	window.scrollTo(0,window.pageYOffset + Otstyp);
-	else {window.scrollTo(0,window.pageYOffset + Otstyp);}*/
-}
+let testimonials = $(".a")
+testimonials.on("click", function() {
+
+})
+
+});
 
 
 
-
-let selector = document.querySelectorAll(".nav__item");
-for (let elem of selector) {
-elem.addEventListener('click', scrollToWindow);
-}
-
-
-
+//console.log(intro.offsetHeight); intro.clientHeight
